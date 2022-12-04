d1f8fc1f494a2b178a137bfb0b1459f490abd758
themosgit <themos360@gmail.com>
2022-12-05 01:48:15 +0200

    Testing

--- header.h
+++ header.h
@@ -6,3 +6,19 @@
 #define OOP_WNV_THE_GAME_HEADER_H
 
 #endif //OOP_WNV_THE_GAME_HEADER_H
+class Game_visuals {
+private:
+    int map_x = 0;
+    int map_y = 0;
+    char** map = NULL;
+public:
+    Game_visuals();
+    void Make_map();
+    void Draw_map();
+
+};
+
+class Map : private Game_visuals {
+public:
+
+};
--- methods.cpp
+++ methods.cpp
@@ -1,3 +1,43 @@
 //
 // Created by Themos Papatheofanous on 4/12/22.
 //
+#include <iostream>
+#include "header.h"
+
+using namespace std;
+
+Game_visuals::Game_visuals() {
+    cout << "Enter Map Dimensions\n"
+         << "X axis : ";
+    cin >> map_x;
+    cout << "Y axis : ";
+    cin >> map_y;
+
+    map = new char* [map_x];
+    for (int i = 0; i < map_x; i++) {
+        map[i] = new char[map_y];
+    }
+
+}
+
+void Game_visuals::Make_map() {
+    for (int i = 0; i < map_x; i++) {
+        for (int j = 0; j < map_y; j++) {
+            if( i == 0 || i == map_x - 1) map[i][j] = '*';
+            else if (j == 0 || j == map_y - 1) map[i][j] = '*';
+            else map[i][j] = ' ';
+        }
+    }
+}
+
+void Game_visuals::Draw_map() {
+    cout << map_x << map_y << endl;
+    for (int i = 0; i < map_x; ++i) {
+        for (int j = 0; j < map_y; ++j) {
+            cout << map[i][j] << "  ";
+        }
+        printf("\n");
+    }
+}
+
+
