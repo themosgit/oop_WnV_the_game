ecd779e7eea0f4820eb3008349bb5286ec4e23bf
themosgit <47158771+themosgit@users.noreply.github.com>
2022-12-22 21:27:41 +0200

    Update methods.cpp

--- methods.cpp
+++ methods.cpp
@@ -116,7 +116,7 @@ void Avatar::choose_team() {
 
 void Avatar::avatar_move(int ud, int lr, Map* map) {
     map->map[x_coord][y_coord] = ' ';
-    if (map->map[x_coord + ud][y_coord] == ' ' || map->map[x_coord + ud][y_coord] == '$' ) x_coord += ud;
+    if (map->map[x_coord + ud][y_coord] == ' ' || map->map[x_coord + ud][y_coord] == '$') x_coord += ud;
     if (map->map[x_coord][y_coord + lr] == ' ' || map->map[x_coord][y_coord + lr] == '$') y_coord += lr;
     if (map ->map[x_coord][y_coord] == '$') incr_filter();;
     map->map[x_coord][y_coord] = team_signate;
@@ -151,6 +151,7 @@ void Avatar::incr_filter() {
 //creature
 creature::creature():Being() {
     health = 4;
+    is_alive = true;
     power = rand() % 3 + 1;
     defence = rand() % 2 + 1;
     giatriko = rand() % 2;
@@ -175,110 +176,147 @@ int creature::get_giatriko(){
     return giatriko;
 }
 
+bool creature::get_alive(){
+    if (is_alive) return true;
+    else return false;
+}
+
 void creature::show() {
     cout << "\nhealth=" << get_health() << "\npower = " << get_power() <<
          "\ndefence = " << get_defence() << "\ngiatriko = " << get_giatriko() <<  endl;
 }
 
-bool creature::is_alive() {
-    if (get_health() > 0)
-        return true;
-    else return false;
-}
-
 void creature::get_healed() {
-    if (is_alive()) {
+    if (is_alive) {
         if (health < 4 && health>0) {
             health++;
             cout << "\n creature got healed \nnew health=" << health << endl;
         }
-        else if (health =4)
+        else if (health = 4)
             cout << "\n creature has max health";
     }
     else cout << "\n cant heal is dead ";
 }
 
-void creature::battle_or_heal(creature* a) {
-    int x = a->get_x() - get_x();
-    int y = a->get_y() - get_y();
-    if (abs(x) == 1 || abs(y) == 1) {
-        if (a->is_vampire() == is_vampire()) {
-            if (giatriko != 0)
-            {		a->get_healed();
-                giatriko--;	 }
-            else cout << "\n not enough giatriko";
-        }
-        else if (a->is_vampire() != is_vampire()) {
-            if (get_power() < a->get_power()) {
-                cout << "\n cant attack is stronger";
-            }
-            else if (get_power() >= a->get_power()) {
-                a->health = a->health - abs(a->get_defence() - get_power());
-                if (a->get_health() <= 0) {
-                    cout << "\n creature is dead";
-                    a->is_alive() == false;
+void creature::battle_or_heal(creature* a, Map* map) {
+    if (a->is_alive && is_alive){
+        int x = a->x_coord - x_coord;
+        int y = a->y_coord - y_coord;
+        if (abs(x) <= 1 && abs(y) <= 1) {
+            if (a->is_vampire() == is_vampire()) {
+                if (giatriko != 0) {
+                    a->get_healed();
+                    giatriko--;
                 }
-                else  cout << "\n attacked creature health is " << a->get_health();
+            }
+            else if (a->is_vampire() != is_vampire()) {
+                if (get_power() < a->get_power()) {
+                    move(map);
+                }
+                else if (get_power() >= a->get_power()) {
+                    a->health = a->health - abs(a->get_defence() - get_power());
+                    if (a->get_health() <= 0) {
+                        a->signate = ' ';
+                        is_alive = false;
+                        map->map[x_coord][y_coord] = ' ';
+                        x_coord = -10;
+                        y_coord = -10;
+                    }
+                    else  cout << a->get_health();
+                }
             }
         }
-    }
-    else {
-        cout << "\n creature is not close ";
-        get_posis();
-        a->get_posis();
     }
 }
 
-void creature::move(){
-    int n;
-    if (is_vampire() == true)
-        n= rand() % 8;
-    else n=rand() % 4;
-    cout << "\n case  " << n;
-    switch (n) {
-        case 0:	//doesnt move
-            break;
-        case 1: // paei deksia x++
-            x_coord++;
-            break;
-        case 2: //paei aristera x--
-            x_coord--;
-            break;
-        case 3: //paei panw y++
-            y_coord++;
-            break;
-        case 4: //paei katv y--
-            y_coord--;
-            break;
-        case 5: //diagwnia panw deksia x++,y++
-            x_coord++;
-            y_coord++;
-            break;
-        case 6: //diagwnia panw aristera y++,x--
-            x_coord--;
-            y_coord++;
-            break;
-        case 7:  //diagvnia katw deksia x++,y--
-            x_coord++;
-            y_coord++;
-            break;
-        case 8:  //diagwnia katw aristera x--, y--
-            x_coord--;
-            y_coord++;
-            break;
-        default:
-            cout << "\n wrong" << endl;
+void creature::move(Map* map){
+    if (is_alive) {
+        int n;
+        if (is_vampire() == true)
+            n = rand() % 8;
+        else n = rand() % 4;
+        switch (n) {
+            case 0:    //doesnt move
+                break;
+            case 1: // paei deksia x++
+                if (map->map[x_coord + 1][y_coord] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord++;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 2: //paei aristera x--
+                if (map->map[x_coord - 1][y_coord] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord--;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 3: //paei panw y++
+                if (map->map[x_coord][y_coord + 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    y_coord++;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 4: //paei katv y--
+                if (map->map[x_coord][y_coord - 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    y_coord--;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 5: //diagwnia panw deksia x++,y++
+                if (map->map[x_coord + 1][y_coord + 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord++;
+                    y_coord++;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 6: //diagwnia panw aristera y++,x--
+                if (map->map[x_coord - 1][y_coord + 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord--;
+                    y_coord++;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 7:  //diagvnia katw deksia x++,y--
+                if (map->map[x_coord + 1][y_coord - 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord++;
+                    y_coord--;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            case 8:  //diagwnia katw aristera x--, y--
+                if (map->map[x_coord - 1][y_coord - 1] == ' ') {
+                    map->map[x_coord][y_coord] = ' ';
+                    x_coord--;
+                    y_coord--;
+                    map->map[x_coord][y_coord] = signate;
+                }
+                break;
+            default:
+                cout << "\n wrong" << endl;
+        }
     }
-    get_posis();
 }
 
 //Vampire
-Vampire::Vampire():creature(){
+Vampire::Vampire(int i,Map* map):creature(){
+    int num = map->Get_mapx()*map->Get_mapy() / 15;
+    x_coord = map->Get_mapx() - num + i - 1;
+    y_coord = map->Get_mapy();
     signate = 'v';
+    map->map[x_coord][y_coord] = signate;
 }
+
 bool Vampire::is_vampire() {
     return true;
 }
+
 bool Vampire::is_werewolf() {
     return false;
 }
@@ -287,19 +325,23 @@ Vampire::~Vampire() {}
 
 
 //werewolf
-Werewolf::Werewolf():creature() {
+Werewolf::Werewolf(int i, Map* map):creature() {
+    int num = map->Get_mapx()*map->Get_mapy() / 15;
+    x_coord = map->Get_mapx() - num + i - 1;
+    y_coord = 1;
     signate = 'w';
+    map->map[x_coord][y_coord] = signate;
 }
+
 Werewolf::~Werewolf(){}
 
 bool Werewolf::is_vampire() {
     return false;
 }
+
 bool Werewolf::is_werewolf() {
     return true;
 }
-
-
 
 
 
